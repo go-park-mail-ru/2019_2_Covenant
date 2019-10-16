4cf0373c0fbfca5367bff06e3e45469be4ba5cc1
Marshal <marshalityy@gmail.com>
2019-10-16 22:04:44 +0300

    session repository

---
+++ pkg/session/repository.go
@@ -0,0 +1,15 @@
+package session
+
+import (
+	"2019_2_Covenant/pkg/models"
+)
+
+/*
+ *	Repository interface represents the session's repository contract
+ */
+
+type Repository interface {
+	Get(value string) (*models.Session, error)
+	Store(newSession *models.Session) error
+	DeleteByID(id uint64) error
+}

ba571b3176422e26ba029fef27755ae731324eb6
Marshality <marshalityy@gmail.com>
2019-11-04 01:17:41 +0300

    fix

--- internal/migrations/sessions/20191103162405_session.up.sql
+++ internal/migrations/sessions/20191103162405_session.up.sql
@@ -1,6 +1,6 @@
 create table sessions (
     id bigserial not null primary key,
-    user_id bigserial not null references users(id) on delete cascade,
+    user_id bigint not null references users(id) on delete cascade,
     expires timestamp not null default now() + interval '24 hours',
     data varchar not null
 )

5832dcfe17422004758399089582a4ebcf03c39a
yuliaplaksina <plaksina-yulia@yandex.ru>
2019-11-06 16:29:13 +0300

    Тестирование track/delivery

---
+++ internal/track/delivery/track_handler_test.go
@@ -0,0 +1,78 @@
+package delivery
+
+import (
+	. "2019_2_Covenant/internal/models"
+	mock "2019_2_Covenant/internal/track/mocks"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+)
+
+//go:generate mockgen -source=../usecase.go -destination=../mocks/mock_usecase.go -package=mock
+
+func TestTrackHandler_GetPopularTracks(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	TUsecase := mock.NewMockRepository(ctrl)
+
+	handler := TrackHandler{TUsecase: TUsecase}
+
+	count := uint64(25)
+
+	t.Run("Test OK", func(t1 *testing.T) {
+		e := echo.New()
+
+		req := httptest.NewRequest(http.MethodGet, "/api/v1", nil)
+		rec := httptest.NewRecorder()
+
+		c := e.NewContext(req, rec)
+		c.SetPath("/tracks/popular")
+
+		//tracks := []*Track{
+		//	{ID: 1, Name: "Still loving you", Duration: "T06:28Z"},
+		//}
+		TUsecase.EXPECT().Fetch(count).Return([]*Track{}, nil)
+		err := handler.GetPopularTracks()(c)
+
+		if err != nil {
+			t1.Fail()
+		}
+
+		body, _ := ioutil.ReadAll(rec.Body)
+		if strings.Trim(string(body), "\n") != `{"body":[]}` {
+			t1.Fail()
+		}
+	})
+
+	t.Run("Error", func(t2 *testing.T) {
+		e := echo.New()
+
+		req := httptest.NewRequest(http.MethodGet, "/api/v1", nil)
+		rec := httptest.NewRecorder()
+
+		c := e.NewContext(req, rec)
+		c.SetPath("/tracks/popular")
+
+		TUsecase.EXPECT().Fetch(count).Return(nil, fmt.Errorf("some error"))
+		err := handler.GetPopularTracks()(c)
+
+		if err != nil {
+			t2.Fail()
+		}
+
+		body, _ := ioutil.ReadAll(rec.Body)
+		fmt.Println(string(body))
+
+		if strings.Trim(string(body), "\n") != `{"error":"internal server error"}` {
+			t2.Fail()
+		}
+	})
+
+}
